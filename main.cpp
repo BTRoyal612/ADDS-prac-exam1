9f03141a4051979c235dc7ce241c7c26a22a2763
gbhoang <gb.hoang02@gmail.com>
2022-03-23 12:07:37 +1030

    prac exam part B

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@ using namespace std;
 
 int main(void) {
     ArtificialPet* bao;
-
     bao = new Furby();
 
     cout << bao->getFullness() << endl;
     cout << bao->isHungry() << endl;
     
     bao->feed();
+    cout << "feed" << endl;
     cout << bao->getFullness() << endl;
 
     bao->feed();
+    cout << "feed" << endl;
     cout << bao->getFullness() << endl;
 
     bao->dance(3);
